a4fc07ac826b13551fe0df6bd9895af8f01d93a9
bowennnn <bowen@molsoft.io>
2023-05-04 23:33:04 -0400

    create remove functions

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+const itemFilter = document.getElementById('filter');
+const clearBtn = document.getElementById('clear');
 
 const createButton = ((classes) => {
   const button = document.createElement('button');
@@ -34,10 +36,46 @@ const addItem = (e) => {
   const button = createButton('remove-item btn-link text-red');
   li.appendChild(button);
 
+  // Add li to the DOM
   itemList.appendChild(li);
+
+  checkUI();
 
   itemInput.value = '';
 }
 
+const removeItem = (e) => {
+  if (e.target.parentElement.classList.contains('remove-item')) {
+    if (confirm('Are you sure?')) {
+      e.target.parentElement.parentElement.remove();
+
+      checkUI();
+    }
+  }
+}
+
+const clearAll = (e) => {
+  itemList.innerHTML = '';
+
+  checkUI();
+}
+
+const checkUI = () => {
+  const items = itemList.querySelectorAll('li');
+  console.log(items);
+
+  if (items.length === 0) {
+    clearBtn.style.display = 'none';
+    itemFilter.style.display = 'none';
+  } else {
+    clearBtn.style.display = 'block';
+    itemFilter.style.display = 'block';
+  }
+}
+
 // Event Listeners
 itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+clearBtn.addEventListener('click', clearAll)
+
+checkUI();
